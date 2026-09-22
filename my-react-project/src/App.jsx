6639d40cc90f545513c10647bf47c304980ec5e3
dhayalamit <amitdhayal6138@gmail.com>
2026-09-22 11:44:27 +0530

    updated react project

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,44 +1,78 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import Game from './Game'
+// import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
+// import Game from './Game'
+// import Login from './login'
+// import Reset from "./Reset";
 
-const App = () => {
- let [apiData,SetApiData]=    useState([])
-  useEffect(()=>{
+// const App = () => {
+//  let [apiData,SetApiData]=    useState([])
+//   useEffect(()=>{
 
-   async function api(){
-      let res=  await   axios.get("http://localhost:4000/")
-      console.log(res.data);
+//    async function api(){
+//       let res=  await   axios.get("http://localhost:3000/")
+//       console.log(res.data);
       
-    }
-    api()
+//     }
+//     api()
 
 
 
-          // fetch("http://localhost:4000/").then((res)=>{
-          //   return res.json()
+//           // fetch("http://localhost:4000/").then((res)=>{
+//           //   return res.json()
 
-          // }).then((data)=>{
-          //   console.log(data);
-          //   SetApiData(data)
-          // })
+//           // }).then((data)=>{
+//           //   console.log(data);
+//           //   SetApiData(data)
+//           // })
 
-  },[])
+//   },[])
+
+//   return (
+//     <div>
+//       {/* {
+//         apiData.map((val)=>{
+//           return(<>
+//           <h1>{val.id}</h1>
+//           <h2>{val.name}</h2>
+//           </>)
+
+//         })
+//       } */}
+
+//       {/* < Game/> */}
+//        {/* <Login /> */}
+
+    
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+import React from "react";
+import Signup from "./Signup";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Dash from "./Dash";
+import Reset from "./Reset";
+
+function App() {
 
   return (
     <div>
-      {/* {
-        apiData.map((val)=>{
-          return(<>
-          <h1>{val.id}</h1>
-          <h2>{val.name}</h2>
-          </>)
+     <Routes>
+      <Route  path="/"  element={<Signup/>}/>
+      <Route  path="/login"  element={<Login/>}/>
+      <Route  path="/dash"  element={<Dash/>}/>
+      <Route  path="/reset/:token"  element={<Reset/>}/>
 
-        })
-      } */}
-      < Game/>
+
+     </Routes>
     </div>
-  )
+  );
+
 }
 
-export default App
+export default App;
---
+++ my-react-project/src/Reset.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Reset = () => {
+  return (
+    <div>
+        <input/>
+        <button>reset</button>
+    </div>
+  )
+}
+
+export default Reset
